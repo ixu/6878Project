37bad4d87b9dd0894f220eb95100505a2821d5ea
Iris Xu <ixu@mit.edu>
2013-11-30 20:34:06 -0500

    updated enrichment analysis rdata files

--- EnrichmentAnalysis/enrichmentAnalysis.R
+++ EnrichmentAnalysis/enrichmentAnalysis.R
@@ -5,14 +5,17 @@ if (length(args)==0) {
 } else {
   eval(parse(text=args[[1]])); # parse first argument: cluster
 }
-load('human-GO.RData')
-file <- paste('input/SetGeneMatrix',cluster,'.csv',sep="")
-setGeneMatrix <- read.table(file, header = FALSE, sep = "\t",row.names = 1)
+load('human-GO.RData');
+file <- paste('input/SetGeneMatrix',cluster,'.csv',sep="");
+print(file);
+setGeneMatrix <- read.table(file, header = FALSE, sep = "\t",row.names = 1);
 source('fisherTestGeneSets.R');
-geneAnnotationMatrix <-as.matrix(GOdata)
-setGeneMatrix <- t(as.matrix(setGeneMatrix))
-backgroundIDset <- colnames(setGeneMatrix)
-minSetSize <- 0
-maxSetSize <- Inf
-results <- fisherTestGeneSets(t(as.matrix(setGeneMatrix)), geneAnnotationMatrix);
+geneAnnotationMatrix <-as.matrix(GOdata);
+setGeneMatrix <- t(as.matrix(setGeneMatrix));
+print(dim(setGeneMatrix))
+backgroundIDset <- colnames(setGeneMatrix);
+minSetSize <- 0;
+maxSetSize <- Inf;
+results <- fisherTestGeneSets(setGeneMatrix, geneAnnotationMatrix);
+print(dim(results$pValueMatrix))
 save(results, file=paste('output/results',cluster,'.RData',sep=""))
